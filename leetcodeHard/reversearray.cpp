a6e8f03d59675bda9f620df536bf4d2be56a99cd
Deepak kumar <deepak19122002@gmail.com>
2024-09-18 23:36:48 +0530

    commit

---
+++ leetcodeHard/reversearray.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5]={1,2,3,4,5};
+
+    int b[5];
+    int j=0;
+    for(int i=4; i>=0; i--)
+    {
+        b[j]=a[i];
+        j++;
+    }
+    for(int i=0; i<5; i++)
+    return 0;
+}
